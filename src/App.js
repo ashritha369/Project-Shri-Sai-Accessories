a0f1868791b486071918d75cb7ea5fd880d235fc
ashritha369 <ashrithagowda666@gmail.com>
2023-07-05 11:01:09 +0530

    set-up-categories.styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import "./categories.styles.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "CROWNS",
+    },
+    {
+      id: 2,
+      title: "DRESSES",
+    },
+    {
+      id: 3,
+      title: "CROWNS+DRESSES",
+    },
+    {
+      id: 4,
+      title: "VARMALAS",
+    },
+    {
+      id: 5,
+      title: "FULL SETS",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div className="category-container" key={id}>
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
