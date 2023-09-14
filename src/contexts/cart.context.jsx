9e11b3913baf5c201b4c3bd44bc003402d19a54d
unknown <ashrithagowda666@gmail.com>
2023-09-14 23:22:22 +0530

    add to cart part 2

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,36 +1,42 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  //find if cartItems contains productsToAdd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+  //if found, increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
 
-const addCartItem=(cartItems,productToAdd)=>{
-   //find if cartItems contains productsToAdd
-
-    //if found, increment quantity
-
-    //return new array with modified cartItems/ new cart item
-}
+  //return new array with modified cartItems/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
-  cartItems:[],
-  addItemToCart:()=>{}
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems,setCartItems]=useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-  const addItemToCart=(productToAdd)=>{
-
-  setCartItems(addCartItem(cartItems,productToAdd))
-
-  }
-  const value = { isCartOpen, setIsCartOpen };
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+  const value = { isCartOpen, setIsCartOpen,addItemToCart,cartItems };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
 
-
-//We know that the format of 'product' is 
+//We know that the format of 'product' is
 /*
 product
 {
