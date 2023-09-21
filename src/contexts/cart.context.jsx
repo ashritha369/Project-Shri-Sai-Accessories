6a5b8d63fec3de1e06acbc2fc010e9c17db44f64
unknown <ashrithagowda666@gmail.com>
2023-09-21 20:34:41 +0530

    worked on total cart count inside cart-icon

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   //find if cartItems contains productsToAdd
@@ -13,7 +13,7 @@ const addCartItem = (cartItems, productToAdd) => {
         : cartItem
     );
   }
- console.log('Correct',[...cartItems, { ...productToAdd, quantity: 1 }])
+  console.log("Correct", [...cartItems, { ...productToAdd, quantity: 1 }]);
 
   //return new array with modified cartItems/ new cart item
   return [...cartItems, { ...productToAdd, quantity: 1 }];
@@ -24,16 +24,26 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  //everytime, the cartItems changes, i want to update the value of cartCount
+  // when somethings changes based upon that change we want to do something: that is when we use useEffect hook
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => {
+      return total + cartItem.quantity;
+    }, 0);
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
-  const value = { isCartOpen, setIsCartOpen,addItemToCart,cartItems };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
 
