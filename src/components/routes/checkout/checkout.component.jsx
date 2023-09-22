7a8c9d244007bb0b5b8d9a2dc066469a01b7004f
unknown <ashrithagowda666@gmail.com>
2023-09-22 17:32:48 +0530

    worked on bringing each cartitems one after the other in checkout page, and worked on logic of increment using already existed addItemToCart in context

--- src/components/routes/checkout/checkout.component.jsx
+++ src/components/routes/checkout/checkout.component.jsx
@@ -1,11 +1,27 @@
-import './checkout.styles.scss';
+import { useContext } from "react";
+import "./checkout.styles.scss";
+import { CartContext } from "../../../contexts/cart.context";
 
-const Checkout=()=>{
-    return(
-        <div>
-            <h1>I am the checkout page</h1>
-        </div>
-    )
-}
+const Checkout = () => {
+  const { cartItems, addItemToCart } = useContext(CartContext);
+  return (
+    <div>
+      <h1>I am the checkout page</h1>
+      {cartItems.map((cartItem) => {
+        const { id, name, imageUrl, price, quantity } = cartItem;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+            <br />
+            <span>decrement</span>
+            <br />
+            <span onClick={() => addItemToCart(cartItem)}>increment</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Checkout;
