e262b040bacbd07d1e0338f11be039d0899815fa
unknown <ashrithagowda666@gmail.com>
2023-10-13 14:56:29 +0530

    displayed categoriesMap details under shop

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,19 +1,26 @@
-import { useContext } from "react";
+import { useContext, Fragment } from "react";
 
 import { CategoriesContext } from "../../../contexts/categories.context";
 import ProductCard from "../../product-card/product-card.component";
 
-import './shop.styles.scss'
+import "./shop.styles.scss";
 
 const Shop = () => {
-  const {categoriesMap}=useContext(CategoriesContext)
+  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="products-container">
-      {/* {products.map((product) => (
-       <ProductCard key={product.id} product={product}/>
-      ))} */}
-    </div>
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
 
-export default Shop
+export default Shop;
