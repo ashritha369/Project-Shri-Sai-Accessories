6ed4e5bf73de729feb300c7adee6c128e7009eee
unknown <ashrithagowda666@gmail.com>
2023-10-17 18:43:23 +0530

    renamed the class

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,7 @@ import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.styles.scss";
 const Directory = ({ categories }) => {
   return (
-    <div className="directory-item-container">
+    <div className="directory-container">
       {categories.map((category) => (
         <DirectoryItem key={category.id} category={category} />
       ))}
