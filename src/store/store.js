160e95b541267d4532946f2eccb81f6ce6a61672
Ashritha H A <ashrithagowda666@gmail.com>
2024-10-25 15:20:06 +0530

    setting up our store

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,5 @@
 import { compass, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+export const store = createStore(rootReducer);
