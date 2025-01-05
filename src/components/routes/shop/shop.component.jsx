fd28af1d77c5837e879a889280087263eb693351
Ashritha H A <ashrithagowda666@gmail.com>
2025-01-05 20:55:55 +0530

    migrated categories context to categories reducer:selector

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,14 +1,45 @@
-import {Routes,Route}from 'react-router-dom';
-import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+
 import "./shop.styles.scss";
+import { getCategoriesAndDocuments } from "../../../utils/firebase/firebase.utils";
+import { setCategoriesMap } from "../../../store/categories/category.action";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      // console.log(categoryMap);
+      dispatch(setCategoriesMap(categoryMap));
+      /*
+        'categoryMap' is an Object we receive it like below
+        Object:{
+            crowns: (8) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+            crowns+dresses: (8) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+            dresses: (8) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+            full sets: (8) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+            varmalas: (8) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+            [[Prototype]]: Object
+          }
+        */
+      /*
+       we learned about Objects (Hash Table data structure) being better for
+        searching for items than Array. 
+        This is a common computing optimization when talking about data structures. 
+       */
+    };
+    getCategoriesMap();
+  }, []);
+
   return (
-<Routes>
-  <Route index element={<CategoriesPreview/>}/>
-  <Route path=":category" element={<Category/>}/>
-</Routes>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
