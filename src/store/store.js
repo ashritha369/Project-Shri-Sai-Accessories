f8fd56ef593c8f2c6e8e0c2b54e4bedff04aa1da
Ashritha H A <ashrithagowda666@gmail.com>
2025-04-07 13:18:18 +0530

    redux:writing our own middleware-demystifying middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,26 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+//below we used concept of chained curry  functions in 'loggerMiddleware'
+/*
+In JavaScript, currying is a functional programming 
+concept where a function doesn't take all its arguments
+at once but instead returns a series of functions
+that each take one argument.
+*/
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log("type", action.type);
+  console.log("payload", action.payload);
+  console.log("currentState:", store.getState());
+  next(action);
+  console.log(`next State`, store.getState());
+};
 // Middleware configuration
-const middleWares = [logger];
+const middleWares = [loggerMiddleware];
 
 // Composing enhancers
 const composedEnhancers = compose(applyMiddleware(...middleWares));
