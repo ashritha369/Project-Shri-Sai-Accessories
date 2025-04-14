373e1c6efeb6fe65fc789d0f8f7eef27c5f92f0a
Ashritha H A <ashrithagowda666@gmail.com>
2025-04-14 23:38:09 +0530

    Asynchronous Redux-Saga: check user session

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import { getCurrentUser } from "./utils/firebase/firebase.utils";
-
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/routes/navigation/navigation.component";
 import Authentication from "./components/routes/authentication/authentication.component";
 import Shop from "./components/routes/shop/shop.component";
 import Checkout from "./components/routes/checkout/checkout.component";
-
+import { checkUserSession } from "./store/user/user.action";
 import "./responsiveness/media-query.styles.scss";
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    getCurrentUser().then((user) => console.log(user));
+    dispatch(checkUserSession);
   }, []);
   return (
     <Routes>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -123,7 +123,7 @@ export const createUserDocumentFromAuth = async (
         console.log("Error creating the user", error.message);
     }
   }
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
