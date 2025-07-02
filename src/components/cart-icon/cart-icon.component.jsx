1b66d455326823065a33c13de1c501f3e1a78629
Ashritha H A <ashrithagowda666@gmail.com>
2025-07-02 11:48:12 +0530

    redux-toolkit:cart to createslice

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ import {
   selectCartCount,
   selectIsCartOpen,
 } from "../../store/cart/cart.selector";
-import { setIsCartOpen } from "../../store/cart/cart.action";
+import { setIsCartOpen } from "../../store/cart/cart.reducer";
 
 import "./cart-icon.styles.scss";
 const CartIcon = () => {
